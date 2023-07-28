7856981733666032603b2f5eb20f1db4525c7580
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-07-28 16:26:02 +0300

    2 tasc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryList: document.querySelector(`.gallery`),
+}
+
+refs.galleryList.insertAdjacentHTML(`beforeend`, createGalleryMarkup(galleryItems));
+
+
+function createGalleryMarkup(array) {
+    return array.reduce((acum, { preview, original, description }) => acum + `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`, ``)
+}
+        
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
